9da7994b5da20022fa0b5001a307ef173434d3ae
claudiaalozano <clau.lozano.perez@gmail.com>
2022-10-31 08:54:19 +0100

    creo la función sarrus para calcular el determinante

--- ejercicio2.py
+++ ejercicio2.py
@@ -1,11 +1,18 @@
-matriz = []
+m = []
 
 def elementos_matriz(n):
     for i in range (n):
-        matriz.append([])
+        m.append([])
         for j in range (n):
             v= int(input("Introduzca valor:"))
-            matriz[i].append(v)
-    return matriz
+            m[i].append(v)
+    return m
+
+
+def sarrus(m):
+    det = ((m[0][0] * m[1][1] * m[2][2]) + (m[0][1] * m[1][2] * m[2][0]) + (m[0][2] * m[1][0] * m[2][1])) - ((m[2][0] * m [1][1] * m[0][2]) + (m[2][1] * m [1][2] * m[0][0]) + (m[2][2] * m[1][0] * m [0][1]))
+    print("El determinante de la matriz por la regla de Srrus es: ", det)
+
 n =3
-print(elementos_matriz(n))
+print(elementos_matriz(n), sarrus(m))
+
